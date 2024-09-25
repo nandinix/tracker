e11a4c89f03ffeacfbba9d9c07ca5ca557da2e4e
nandinix <138158216+nandinix@users.noreply.github.com>
2024-09-25 09:36:45 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,15 +6,7 @@ import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCnt9BCLVReBb_U0EsscYE7PlZNbUOR1lc",
-  authDomain: "tracker-64e09.firebaseapp.com",
-  projectId: "tracker-64e09",
-  storageBucket: "tracker-64e09.appspot.com",
-  messagingSenderId: "962927533568",
-  appId: "1:962927533568:web:c2637d459453dcd17482e5",
-  measurementId: "G-7XFFPHMHC8"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
